6750eabb72640d9ae9952ee42e93736dd78b2ecb
Brenda Rayane <brenda.rayane.0018@gmail.com>
2025-03-18 15:15:26 -0300

    feat: file editor

---
+++ src/processData.js
@@ -0,0 +1,20 @@
+import fs from 'fs/promises';
+
+async function processData() {
+    try {
+        const rawData = await fs.readFile('data.json', 'utf8');
+        const data = JSON.parse(rawData);
+
+        const updatedData = data.map(person => ({
+            ...person,
+            idade: person.idade + 1
+        }));
+
+        await fs.writeFile('updatedData.json', JSON.stringify(updatedData, null, 2), 'utf8');
+        console.log('Arquivo updatedData.json criado com sucesso!');
+    } catch (error) {
+        console.error('Erro ao processar o arquivo:', error);
+    }
+}
+
+processData();
